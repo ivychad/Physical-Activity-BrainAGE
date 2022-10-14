e2cf58c39cdba788a765ed2d2a707a1d5a581184
ivychad <hanna.hoogen@hotmail.de>
2022-10-14 15:36:07 +0000

    Includes different models and delta

--- RidgeRegression.py
+++ RidgeRegression.py
@@ -2,24 +2,40 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import LassoCV, RidgeCV
+from sklearn.svm import SVR
 from sklearn.experimental import enable_iterative_imputer 
 from sklearn.impute import IterativeImputer
 from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import *
+from random import seed
+from scipy import stats
+
+seed(888)
 
 # load data from csv and convert to array
 brain_test = pd.read_pickle("test_brain.pkl")
 brain_train = pd.read_pickle("train_brain.pkl")
-# main_test = pd.read_csv("test_main.csv")
-# main_train = pd.read_csv("train_main.csv")
+main_test = pd.read_pickle("test_main.pkl")
+main_train = pd.read_pickle("train_main.pkl")
 brain_test = pd.DataFrame(brain_test)
 brain_train = pd.DataFrame(brain_train)
-# main_test = pd.DataFrame(main_test)
-# main_train = pd.DataFrame(main_train)
+main_test = pd.DataFrame(main_test)
+main_train = pd.DataFrame(main_train)
 
 # delete rows that have a missing value for Age
 brain_train = brain_train.drop(brain_train[np.isnan(brain_train.iloc[:,-1])].index)
 brain_test = brain_test.drop(brain_test[np.isnan(brain_test.iloc[:,-1])].index)
+main_test = main_test.drop(brain_test[np.isnan(brain_test.iloc[:,-1])].index)
+main_train = main_train.drop(brain_train[np.isnan(brain_train.iloc[:,-1])].index)
+
+
+
+# exclude people with long standing illness diability or infirmity
+main_train = main_train.drop(main_train[main_train.iloc[:,967]!= "No"].index)
+main_test = main_test.drop(main_test[main_test.iloc[:,967]!= "No"].index)
+brain_train = brain_train.drop(main_train[main_train.iloc[:,967]!= "No"].index)
+brain_test = brain_test.drop(main_test[main_test.iloc[:,967]!= "No"].index)
 
 
 # Freesurfer ASEG
@@ -55,21 +71,36 @@ Y_train = brain_training.iloc[:,-1]
 X_test = brain_testing.iloc[:,:-1]
 Y_test = brain_testing.iloc[:,-1]
 
+scaler = StandardScaler()
+
+X_train_std = scaler.fit_transform(X_train)
+X_test_std = scaler.fit_transform(X_test)
+
 # X = brain_training.iloc[:,:-1]
 # Y = brain_training.iloc[:,-1]
 
 # # # randomly split data into training and testing set
 # X_train, X_test, Y_train, Y_test = train_test_split(X,Y, random_state=0)
 
-# ridge regression initialization
-ridge = Ridge(alpha= 0.5)
+alpha_parameters = np.power(10,np.linspace(start=-3, stop=5, num=100))
+
+# model initialization - options to use Lasso, Ridge or SVM
+#model = LassoCV(alphas = alpha_parameters, max_iter=100000)
+#model = RidgeCV(alphas = alpha_parameters)
+model = SVR(kernel = 'rbf')
 
 # train model
-ridge.fit(X_train,Y_train)
+model.fit(X_train_std,Y_train)
 
 # get predicted values for test set
-y_pred = ridge.predict(X_test)
-print(y_pred)
+y_pred = model.predict(X_test_std)
+
+# calculate brain age gap
+brain_age_delta = y_pred-Y_test
+
+# check whether brain age gap and age are correlated
+correlation = stats.pearsonr(brain_age_delta, Y_test)
+print(correlation)
 
 # get mean absolute error (MAE)
 print(mean_absolute_error(Y_test,y_pred))
@@ -82,4 +113,11 @@ plt.xticks(range(40,90,5))
 plt.yticks(range(40,110,5))
 plt.ylabel('Predicted Age')
 plt.xlabel('Age')
+plt.show()
+
+plt.figure()
+plt.scatter(Y_test,brain_age_delta)
+plt.axline((60,5), slope=-0.69, color="r")
+plt.ylabel("Brain Age Delta")
+plt.xlabel("Age")
 plt.show()
